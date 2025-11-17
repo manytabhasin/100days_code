da942d73ce5d9af4e7bf331c7a9dd2000d10d955
Manyta Bhasin <bhasinmanyta2007@gmail.com>
2025-11-17 22:10:51 +0530

    main

--- lab.c
+++
@@ -1,9 +0,0 @@
-//to check a perfect number 
-rxample: 28 = 1 + 2 + 4 + 7 + 14 //
-#include <stdio.h>
-int main() {
-    int num, sum = 0;
-    printf("Enter a number: ");
-    scanf("%d", &num);
-
-    for(int i = 1; i <= num / 2; 
